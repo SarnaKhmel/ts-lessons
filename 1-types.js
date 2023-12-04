a837b0048c589eb71b39342e091b8aa0c9b9712e
sarnakhmel <OlSa@olsa.local>
2023-12-04 13:24:51 +0200

    types

--- 1-types.js
+++ 1-types.js
@@ -1,2 +1,33 @@
+// Змінні в TS 
+// Загалом в TS статична та сильна типізація (тобто для змінної ми не можемо задавати новий тип) 
+// проте це можна обійти використавши any, що задати тип  динамічно (що робити не варто).
+// В TS структурна типізація.
+// Number 
+var numb1 = 1; // Явна типізація
+// String
+var str1 = "hello"; // Явна типізація
+// Boolean
+var bool1 = true; // Явна типізація
+// Symbol    (Для унікальних та незмінних значень)
+// const sym1: symbol = Symbol("key"); // Явна типізація
+// Null Undefined
+var null1 = null;
+var undefined1 = undefined;
+// BigInt   (Великі цілі числа)
+//const bigInt1: bigint = 12345678901234567800000n; 
+// Void
+// Потрібен у випадках якщо ми не повертаємо значень.
+var check = function (a) { };
+//Never
+// Ніколи не повертаємо значень (використовується у функціях з помилками)
+var error = function (message) {
+    throw new Error(message);
+};
+// Unknown 
+//(коли не відомий тип даних який ми оримуємо, ПОТРІБНА ПЕРЕВІРКА).
+// const unknown1: unknown = await fetch()
+// Any
+//(коли не відомий тип даних який ми оримуємо, НЕ ПОТРІБНА  ПЕРЕВІРКА).
+//const any1: any = await fetch()
 var str = "hello";
 console.log(str);
--- 1-types.ts
+++ 1-types.ts
@@ -1,2 +1,52 @@
+// Змінні в TS 
+// Загалом в TS статична та сильна типізація (тобто для змінної ми не можемо задавати новий тип) 
+// проте це можна обійти використавши any, що задати тип  динамічно (що робити не варто).
+
+// В TS структурна типізація.
+
+// Number 
+const numb1: number = 1;  // Явна типізація
+
+// String
+const str1: string = "hello"; // Явна типізація
+
+// Boolean
+const bool1: boolean = true; // Явна типізація
+
+// Symbol    (Для унікальних та незмінних значень)
+
+// const sym1: symbol = Symbol("key"); // Явна типізація
+
+// Null Undefined
+const null1: null = null;
+const undefined1: undefined = undefined;
+
+// BigInt   (Великі цілі числа)
+//const bigInt1: bigint = 12345678901234567800000n; 
+
+// Void
+// Потрібен у випадках якщо ми не повертаємо значень.
+const check = (a: number): void => {}
+
+//Never
+// Ніколи не повертаємо значень (використовується у функціях з помилками)
+const error = (message: string) : never => {
+    throw new Error(message);
+}
+
+// Unknown 
+//(коли не відомий тип даних який ми оримуємо, ПОТРІБНА ПЕРЕВІРКА).
+// const unknown1: unknown = await fetch()
+
+// Any
+//(коли не відомий тип даних який ми оримуємо, НЕ ПОТРІБНА  ПЕРЕВІРКА).
+//const any1: any = await fetch()
+
+
+
+
+
+
+
 const str: string = "hello";
 console.log(str);
